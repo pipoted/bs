466e24202b6bdbc1e5a83b549888c846083bde47
pipoted <32660879+pipoted@users.noreply.github.com>
2019-03-27 16:25:14 +0800

    51job spider version one

---
+++ spider/all_spider/51_job_spider.py
@@ -0,0 +1,4 @@
+# !/usr/bin/python
+# -*- coding: <utf-8> -*-
+__author__ = 'xiao'
+__date__ = '2019/3/26 4:45 PM'
---
+++ spider/all_spider/boss_spider.py
@@ -0,0 +1,4 @@
+# !/usr/bin/python
+# -*- coding: <utf-8> -*-
+__author__ = 'xiao'
+__date__ = '2019/3/27 2:08 AM'
